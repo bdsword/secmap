5db10727b0ce87b2fdec079b4cae7d91adc44541
simon435500 <simon435500@gmail.com>
2016-12-29 14:20:46 -0800

    modify analyzer sample

--- examples/analyzers/HELLO_WORLD/hello_world.rb
+++ examples/analyzers/HELLO_WORLD/hello_world.rb
@@ -1,13 +1,4 @@
 #!/usr/bin/env ruby
 
-dll=`ls | grep .dll`
-exe=`ls | grep .exe`
 `echo HELLO WORLD! > HELLO_WORLD.log`
-
-dll.each_line do |line|
-	`echo "file #{line.strip!}" >> HELLO_WORLD.log`
-end
-
-exe.each_line do |line|
-        `echo "file #{line.strip!}" >> HELLO_WORLD.log`
-end
+`echo "file #{ARGV[0]}" >> HELLO_WORLD.log`

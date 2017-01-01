eaa279e4116cdbba2c4691306a9ca99c8473b3ea
simon435500 <simon435500@users.noreply.github.com>
2017-01-01 23:22:58 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 
-apt update
-apt install ruby=1:2.3.0+1
-gem install redis -v 3.3.2
-gem install cassandra-driver -v 3.1.0
-gem install docker-api -v 1.33.1
+apt update && \
+apt install -y ruby=1:2.3.0+1 ruby-dev=1:2.3.0+1 gcc=4:5.3.1-1ubuntu1 make=4.1-6 && \
+gem install redis -v 3.3.2 && \
+gem install cassandra-driver -v 3.1.0 && \
+gem install docker-api -v 1.33.1 && \
 gem install sys-filesystem -v 1.1.7
-
-./lib/cassandraDocker init
-./lib/redisDocker init

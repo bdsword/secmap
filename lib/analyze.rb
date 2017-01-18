b252110770ffa519e083cf3e412e698087fcc7bb
simon435500 <simon435500@gmail.com>
2017-01-18 14:26:23 +0800

    sample file path slash compensatory

--- lib/analyze.rb
+++ lib/analyze.rb
@@ -38,7 +38,8 @@ class Analyze
 			STDERR.puts "File #{taskuid} not found!!!!"
 			return nil
 		else
-			path = res['path'].sub(SAMPLE, '/sample')
+			path = res['path'].sub(SAMPLE, '/sample/')
+			path = File.expand_path(path)
 			return path
 		end
 	end

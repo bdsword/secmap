1f572e9e708829a93d793d1bb3aa7f7b487325bb
simon435500 <simon435500@gmail.com>
2017-02-02 15:52:25 +0800

    fix float nan bug

--- lib/analyze.rb
+++ lib/analyze.rb
@@ -47,22 +47,27 @@ class Analyze
 	def analyze(file_path)
 		result = ""
 		max_memory = 0
-		max_cpu = 0
+		max_cpu = 0.0
 		last_cputime = 0
 		last_etimes = 0
+		total_time = 0
 		IO.popen("/analyze #{file_path}", "r+") { |f|
 			while true
 				begin
-					memory, cputime, etimes = `ps -o vsz,cputime,etimes -p #{Process::pid}`.chomp.split("\n").last.split(' ')
+					memory, cputime, etimes = `ps -o vsz,cputime,etimes -p #{f.pid}`.chomp.split("\n").last.split(' ')
 					memory = memory.strip.to_i
 					etimes = etimes.strip.to_i
 					h,m,s = cputime.split(':')
 					cputime = h.strip.to_i*60*60 + m.strip.to_i*60 + s.strip.to_i
 					cpu = (cputime - last_cputime) / (etimes - last_etimes).to_f
+					if cpu.nan?
+						cpu = 0.0
+					end
 					max_memory = [memory, max_memory].max
 					max_cpu = [cpu, max_cpu].max
 					result += f.read_nonblock(4096)
 				rescue IO::WaitReadable
+					total_time += 1
 					sleep 1
 					next
 				rescue EOFError
@@ -71,7 +76,7 @@ class Analyze
 			end
 		}
 		log = File.new('/log/usage.log', 'a')
-		log.write("#{file_path}:#{max_memory}:#{max_cpu}\n")
+		log.write("#{file_path}:#{max_memory}:#{max_cpu*100}:#{total_time}\n")
 		log.close
 		return result
 	end
